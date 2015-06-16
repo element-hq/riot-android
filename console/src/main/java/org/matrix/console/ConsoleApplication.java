ebe83324b4a760c4ba4abe3c00f88f03e4684476
yannick <yannick@matrix.org>
2015-06-16 15:45:46 +0200

    Add GA supports.

--- console/src/main/java/org/matrix/console/ConsoleApplication.java
+++ console/src/main/java/org/matrix/console/ConsoleApplication.java
@@ -18,11 +18,22 @@ package org.matrix.console;
 import android.app.Activity;
 import android.app.Application;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.text.TextUtils;
 import android.util.Log;
+
+import com.google.android.gms.analytics.ExceptionParser;
+import com.google.android.gms.analytics.ExceptionReporter;
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
 
 import org.matrix.console.activity.CommonActivityUtils;
 import org.matrix.console.contacts.ContactsManager;
 import org.matrix.console.contacts.PIDsRetriever;
+import org.matrix.console.ga.Analytics;
 import org.matrix.console.services.EventStreamService;
 
 import java.io.Console;
@@ -33,16 +44,32 @@ import java.util.TimerTask;
  * The main application injection point
  */
 public class ConsoleApplication extends Application {
+    private static final String LOG_TAG = "ConsoleApplication";
+
     private Timer mActivityTransitionTimer;
     private TimerTask mActivityTransitionTimerTask;
     public boolean isInBackground = true;
     private final long MAX_ACTIVITY_TRANSITION_TIME_MS = 2000;
+
+    // google analytics
+    private static GoogleAnalytics sGoogleAnalytics;
+    private int VERSION_BUILD = -1;
+    private String VERSION_STRING = "";
 
     @Override
     public void onCreate() {
         super.onCreate();
         mActivityTransitionTimer = null;
         mActivityTransitionTimerTask = null;
+
+        try {
+            PackageInfo pinfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            VERSION_BUILD = pinfo.versionCode;
+            VERSION_STRING = pinfo.versionName;
+        }
+        catch (PackageManager.NameNotFoundException e) {}
+
+        initGoogleAnalytics();
 
         // reset the application badge at application launch
         CommonActivityUtils.updateUnreadMessagesBadge(this, 0);
@@ -64,7 +91,7 @@ public class ConsoleApplication extends Application {
             public void run() {
                 ConsoleApplication.this.isInBackground = true;
 
-                // supend the events thread if the client uses GCM
+                // suspend the events thread if the client uses GCM
                 if (Matrix.getInstance(ConsoleApplication.this).getSharedGcmRegistrationManager().useGCM()) {
                     CommonActivityUtils.pauseEventStream(ConsoleApplication.this);
                 }
@@ -119,6 +146,41 @@ public class ConsoleApplication extends Application {
         }
 
         return true;
+    }
+
+    private void initGoogleAnalytics() {
+        // pull tracker resource ID from res/values/analytics.xml
+        int trackerResId = getResources().getIdentifier("ga_trackingId", "string", getPackageName());
+        if (trackerResId == 0) {
+            Log.e(LOG_TAG, "Unable to find tracker id for Google Analytics");
+            return;
+        }
+
+        String trackerId = getString(trackerResId);
+        Log.d(LOG_TAG, "Tracker ID: "+trackerId);
+        // init google analytics with this tracker ID
+        if (!TextUtils.isEmpty(trackerId)) {
+            Analytics.initialiseGoogleAnalytics(this, trackerId, new ExceptionParser() {
+                @Override
+                public String getDescription(String threadName, Throwable throwable) {
+                    StringBuilder b = new StringBuilder();
+                    b.append((BuildConfig.DEBUG ? "DEBUG"  : "RELEASE") + " : " + VERSION_BUILD);
+                    b.append(" ").append(VERSION_STRING).append(" Thread: ");
+                    b.append(threadName);
+
+                    Activity a = ConsoleApplication.getCurrentActivity();
+                    if (a != null) {
+                        b.append(", Activity:");
+                        b.append(a.getLocalClassName());
+                    }
+
+                    b.append(", Exception: ");
+                    b.append(Analytics.getStackTrace(throwable));
+                    Log.e("FATAL EXCEPTION", b.toString());
+                    return b.toString();
+                }
+            });
+        }
     }
 }
 
