9362ec9f866b4bc8618998a4619224e01aba31e8
yannick <yannick@matrix.org>
2016-01-21 11:02:52 +0100

    sync_v2

--- console/src/main/java/im/vector/activity/VectorRoomActivity.java
+++ console/src/main/java/im/vector/activity/VectorRoomActivity.java
@@ -181,7 +181,7 @@ public class VectorRoomActivity extends MXCActionBarActivity {
     private MXEventListener mEventListener = new MXEventListener() {
 
         @Override
-        public void onDeleteRoom(String roomId) {
+        public void onLeaveRoom(String roomId) {
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
@@ -202,20 +202,6 @@ public class VectorRoomActivity extends MXCActionBarActivity {
                             || Event.EVENT_TYPE_STATE_ROOM_MEMBER.equals(event.type)) {
                         setTitle(VectorUtils.getRoomDisplayname(VectorRoomActivity.this, mSession, mRoom));
                         updateMenuEntries();
-
-                        // check if the user does not leave the room with another client
-                        if (Event.EVENT_TYPE_STATE_ROOM_MEMBER.equals(event.type) && mMyUserId.equals(event.stateKey)) {
-                            RoomMember myMember = mRoom.getMember(mMyUserId);
-
-                            if ((null != myMember) && (RoomMember.MEMBERSHIP_LEAVE.equals(myMember.membership))) {
-                                runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        VectorRoomActivity.this.finish();
-                                    }
-                                });
-                            }
-                        }
                     }
                     else if (Event.EVENT_TYPE_STATE_ROOM_TOPIC.equals(event.type)) {
                         Log.d(LOG_TAG, "Updating room topic.");
