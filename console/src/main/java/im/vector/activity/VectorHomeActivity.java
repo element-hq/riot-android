a335b936e47b5a9fd9e149dfdcb8ee93d9992004
ylecollen <ylecollen@amdocs.com>
2016-02-03 17:25:32 +0100

    add_settings -> the sliding menu was not properly refreshed when the account info was updated.

--- console/src/main/java/im/vector/activity/VectorHomeActivity.java
+++ console/src/main/java/im/vector/activity/VectorHomeActivity.java
@@ -32,6 +32,7 @@ import android.widget.TextView;
 
 import org.matrix.androidsdk.MXSession;
 import org.matrix.androidsdk.call.MXCallsManager;
+import org.matrix.androidsdk.data.MyUser;
 import org.matrix.androidsdk.data.Room;
 import org.matrix.androidsdk.data.RoomAccountData;
 import org.matrix.androidsdk.data.RoomState;
@@ -212,6 +213,11 @@ public class VectorHomeActivity extends MXCActionBarActivity implements VectorRo
 
         MXEventListener listener = new MXEventListener() {
             private boolean mInitialSyncComplete = false;
+
+            @Override
+            public void onAccountInfoUpdate(MyUser myUser) {
+                refreshSlidingMenu();
+            }
 
             @Override
             public void onInitialSyncComplete() {
