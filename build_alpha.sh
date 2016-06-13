565ccac683826859215ed49887581aedc8fe7aa1
ylecollen <ylecollen@amdocs.com>
2016-06-13 08:03:02 +0200

    Rename flavor from vectorApp to app

--- build_alpha.sh
+++ build_alpha.sh
@@ -2,5 +2,5 @@
 rm *.apk
 ./gradlew clean build
 #cp app/build/outputs/apk/app-alpha-matrixorg.apk ./alpha.apk
-cp vector/build/outputs/apk/vector-vectorApp-matrixorg.apk ./vectorAppGooglePlay.apk
-cp vector/build/outputs/apk/vector-vectorAppFDroid-matrixorg.apk ./vectorAppFDroid.apk
+cp vector/build/outputs/apk/vector-app-matrixorg.apk ./vectorAppGooglePlay.apk
+cp vector/build/outputs/apk/vector-appFDroid-matrixorg.apk ./vectorAppFDroid.apk
