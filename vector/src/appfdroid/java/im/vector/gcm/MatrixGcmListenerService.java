a588e0d2b18b31add31744de91acc50b4f076348
ylecollen <ylecollen@amdocs.com>
2016-06-15 14:38:44 +0200

    split_fdroid -> first split

---
+++ vector/src/appfdroid/java/im/vector/gcm/MatrixGcmListenerService.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package im.vector.gcm;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.gcm.GcmListenerService;
+
+import im.vector.Matrix;
+import im.vector.VectorApp;
+import im.vector.activity.CommonActivityUtils;
+
+public class MatrixGcmListenerService extends GcmListenerService {
+
+    private static final String LOG_TAG = "GcmListenerService";
+    private Boolean mCheckLaunched = false;
+    private android.os.Handler mUIhandler = null;
+
+    /**
+     * Called when message is received.
+     *
+     * @param from SenderID of the sender.
+     * @param data Data bundle containing message data as key/value pairs.
+     *             For Set of keys use data.keySet().
+     */
+    @Override
+    public void onMessageReceived(final String from, final Bundle data) {
+
+        if (null == mUIhandler) {
+            mUIhandler = new android.os.Handler(VectorApp.getInstance().getMainLooper());
+        }
+
+        mUIhandler.post(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(LOG_TAG, "## onMessageReceived() --------------------------------");
+                for (String key : data.keySet()) {
+                    Log.d(LOG_TAG, "## onMessageReceived() >>> " + key + " : " + data.get(key));
+                }
+                int unreadCount = 0;
+
+                Object unreadCounterAsVoid = data.get("unread");
+                if (unreadCounterAsVoid instanceof String) {
+                    unreadCount = Integer.parseInt((String) unreadCounterAsVoid);
+                }
+
+                // update the badge counter
+                CommonActivityUtils.updateBadgeCount(getApplicationContext(), unreadCount);
+
+                GcmRegistrationManager gcmManager = Matrix.getInstance(getApplicationContext()).getSharedGcmRegistrationManager();
+
+                if (!gcmManager.isNotificationsAllowed()) {
+                    Log.d(LOG_TAG, "## onMessageReceived() : teh notifications are disabled");
+                    return;
+                }
+
+                if (!gcmManager.isBackgroundSyncAllowed() && VectorApp.isAppInBackground()) {
+                    Log.d(LOG_TAG, "## onMessageReceived() : the background sync is disabled");
+                    return;
+                }
+
+                // check if the application has been launched once
+                // the first GCM event could have been triggered whereas the application is not yet launched.
+                // so it is required to create the sessions and to start/resume event stream
+                if (!mCheckLaunched && (null != Matrix.getInstance(getApplicationContext()).getDefaultSession())) {
+                    CommonActivityUtils.startEventStreamService(MatrixGcmListenerService.this);
+                    mCheckLaunched = true;
+                }
+
+                CommonActivityUtils.catchupEventStream(MatrixGcmListenerService.this);
+            }
+        });
+    }
+}
