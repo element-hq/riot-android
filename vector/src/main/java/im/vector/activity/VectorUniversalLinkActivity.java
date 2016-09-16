366e9d0c7b9be6c74f33b2d8e2434c0a1eb3c69b
ylecollen <ylecollen@amdocs.com>
2016-09-16 17:00:03 +0200

    release_v0.5.0
    Link a new email was broken.

--- vector/src/main/java/im/vector/activity/VectorUniversalLinkActivity.java
+++ vector/src/main/java/im/vector/activity/VectorUniversalLinkActivity.java
@@ -18,10 +18,29 @@ package im.vector.activity;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.widget.Toast;
 
+import org.matrix.androidsdk.HomeserverConnectionConfig;
+import org.matrix.androidsdk.rest.callback.ApiCallback;
+import org.matrix.androidsdk.rest.model.MatrixError;
+import org.matrix.androidsdk.ssl.Fingerprint;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import im.vector.LoginHandler;
+import im.vector.Matrix;
+import im.vector.R;
 import im.vector.receiver.VectorRegistrationReceiver;
 import im.vector.receiver.VectorUniversalLinkReceiver;
 
@@ -42,10 +61,22 @@ public class VectorUniversalLinkActivity extends Activity {
             // dispatch on the right receiver
             if (VectorRegistrationReceiver.SUPPORTED_PATH_ACCOUNT_EMAIL_VALIDATION.equals(getIntent().getData().getPath())) {
 
-                // logout current session, before starting any mail validation
-                // to have the LoginActivity always in a "no credentials state".
-                CommonActivityUtils.logout(this, false);
-                intentAction = VectorRegistrationReceiver.BROADCAST_ACTION_REGISTRATION;
+                Uri intentUri = getIntent().getData();
+                // account registration URL set in a mail:
+                HashMap<String, String> mailRegParams = VectorRegistrationReceiver.parseMailRegistrationLink(intentUri);
+
+                // when there is a next link, assume it is a new account creation
+                if (mailRegParams.containsKey(VectorRegistrationReceiver.KEY_MAIL_VALIDATION_NEXT_LINK) || (null == Matrix.getInstance(this).getDefaultSession())) {
+                    // logout current session, before starting any mail validation
+                    // to have the LoginActivity always in a "no credentials state".
+                    CommonActivityUtils.logout(this, false);
+                    intentAction = VectorRegistrationReceiver.BROADCAST_ACTION_REGISTRATION;
+                } else {
+                    intentAction = null;
+                    // display a spinner while binding the email
+                    setContentView(R.layout.activity_vector_universal_link_activity);
+                    emailBinding(intentUri, mailRegParams);
+                }
             } else {
                 intentAction = VectorUniversalLinkReceiver.BROADCAST_ACTION_UNIVERSAL_LINK;
             }
@@ -53,10 +84,89 @@ public class VectorUniversalLinkActivity extends Activity {
             Log.e(LOG_TAG,"## onCreate(): Exception - Msg="+ex.getMessage());
         }
 
-        Intent myBroadcastIntent = new Intent(intentAction, getIntent().getData());
-        sendBroadcast(myBroadcastIntent);
+        if (null != intentAction) {
+            Intent myBroadcastIntent = new Intent(intentAction, getIntent().getData());
+            sendBroadcast(myBroadcastIntent);
+            finish();
+        }
+    }
 
+    /**
+     * Email binding management
+     * @param uri the uri.
+     * @param aMapParams the parsed params
+     */
+    private void emailBinding(Uri uri, HashMap<String, String> aMapParams) {
+        Log.d(LOG_TAG,"## emailBinding()");
 
-        finish();
+        String ISUrl = uri.getScheme() + "://" + uri.getHost();
+
+        final HomeserverConnectionConfig homeServerConfig = new HomeserverConnectionConfig(Uri.parse(ISUrl), Uri.parse(ISUrl), null, new ArrayList<Fingerprint>(), false);
+
+        String token = aMapParams.get(VectorRegistrationReceiver.KEY_MAIL_VALIDATION_TOKEN);
+        String clientSecret = aMapParams.get(VectorRegistrationReceiver.KEY_MAIL_VALIDATION_CLIENT_SECRET);
+        String identityServerSessId = aMapParams.get(VectorRegistrationReceiver.KEY_MAIL_VALIDATION_IDENTITY_SERVER_SESSION_ID);
+
+        final LoginHandler loginHandler = new LoginHandler();
+
+        loginHandler.submitEmailTokenValidation(getApplicationContext(), homeServerConfig, token, clientSecret, identityServerSessId, new ApiCallback<Map<String,Object>>() {
+
+            private void bringAppToForeground() {
+                final ActivityManager am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+                List<ActivityManager.RunningTaskInfo> tasklist = am.getRunningTasks(100);
+
+                if (!tasklist.isEmpty()) {
+                    int nSize = tasklist.size();
+                    for (int i = 0; i < nSize; i++) {
+                        final ActivityManager.RunningTaskInfo taskinfo = tasklist.get(i);
+                        if (taskinfo.topActivity.getPackageName().equals(getApplicationContext().getPackageName())) {
+                            Log.d(LOG_TAG, "## emailBinding(): bring the app in foreground.");
+                            am.moveTaskToFront(taskinfo.id, 0);
+                        }
+                    }
+                }
+
+                finish();
+            }
+
+            private void errorHandler(final String errorMessage) {
+                VectorUniversalLinkActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), errorMessage, Toast.LENGTH_LONG).show();
+                        bringAppToForeground();
+                    }
+                });
+            }
+
+            @Override
+            public void onSuccess(Map<String,Object> mapResp) {
+                VectorUniversalLinkActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.d(LOG_TAG, "## emailBinding(): succeeds.");
+                        bringAppToForeground();
+                    }
+                });
+            }
+
+            @Override
+            public void onNetworkError(Exception e) {
+                Log.d(LOG_TAG, "## emailBinding(): onNetworkError() Msg=" + e.getLocalizedMessage());
+                errorHandler(getString(R.string.login_error_unable_register) + " : " + e.getLocalizedMessage());
+            }
+
+            @Override
+            public void onMatrixError(MatrixError e) {
+                Log.d(LOG_TAG, "## emailBinding(): onMatrixError() Msg=" + e.getLocalizedMessage());
+                errorHandler(getString(R.string.login_error_unable_register) + " : " + e.getLocalizedMessage());
+            }
+
+            @Override
+            public void onUnexpectedError(Exception e) {
+                Log.d(LOG_TAG, "## emailBinding(): onUnexpectedError() Msg=" + e.getLocalizedMessage());
+                errorHandler(getString(R.string.login_error_unable_register) + " : " + e.getLocalizedMessage());
+            }
+        });
     }
 }
