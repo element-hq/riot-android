8e341371b1e4d79251043ab7fcdc6e249e42a66d
ylecollen <ylecollen@amdocs.com>
2017-01-23 11:06:04 +0100

    trigger_email_lookup_on_use Trigger the PIDs lookup only on demand to save data.

--- vector/src/main/java/im/vector/contacts/ContactsManager.java
+++ vector/src/main/java/im/vector/contacts/ContactsManager.java
@@ -83,11 +83,13 @@ public class ContactsManager {
     // set to true when there is a pending
     private static boolean mIsRetrievingPids = false;
     private static boolean mArePidsRetrieved = false;
+    // Trigger another PIDs retrieval when there is a valid data connection.
+    private static boolean mRetryPIDsRetrievalOnConnect = false;
 
     private static final IMXNetworkEventListener mNetworkConnectivityReceiver = new IMXNetworkEventListener() {
         @Override
         public void onNetworkConnectionUpdate(boolean isConnected) {
-            if (isConnected) {
+            if (isConnected && mRetryPIDsRetrievalOnConnect) {
                 retrievePids();
             }
         }
@@ -131,6 +133,7 @@ public class ContactsManager {
         public void onFailure(String accountId) {
             mIsRetrievingPids = false;
             mArePidsRetrieved = false;
+            mRetryPIDsRetrievalOnConnect = true;
             Log.d(LOG_TAG, "## fail to retrieve the PIDs");
         }
 
@@ -144,6 +147,7 @@ public class ContactsManager {
 
             Log.d(LOG_TAG, "## Retrieve IPDs successfully");
 
+            mRetryPIDsRetrievalOnConnect = false;
             mIsRetrievingPids = false;
             mArePidsRetrieved = true;
 
@@ -491,7 +495,7 @@ public class ContactsManager {
                     mIsRetrievingPids = false;
                     mArePidsRetrieved = false;
 
-                    retrievePids();
+                    // the PIDs retrieval is done on demand.
                 }
 
                 if (null != mListeners) {
