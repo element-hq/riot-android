caf0babaaaed9d78be7c7de0432661fc8677f4bb
ylecollen <ylecollen@amdocs.com>
2017-03-16 17:34:35 +0100

    add_unread_badge Add the background color management according to the notification statuses

--- vector/src/main/java/im/vector/view/UnreadCounterBadgeView.java
+++ vector/src/main/java/im/vector/view/UnreadCounterBadgeView.java
@@ -18,6 +18,7 @@ package im.vector.view;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.support.annotation.IntDef;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.ImageView;
@@ -26,11 +27,24 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 import im.vector.R;
 
 public class UnreadCounterBadgeView extends RelativeLayout {
 
+    // the background settings
+    public static final int HIGHLIGHTED = 0;
+    public static final int NOTIFIED = 1;
+    public static final int DEFAULT = 2;
+
+    @IntDef({HIGHLIGHTED, NOTIFIED, DEFAULT})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Status {}
+
     private TextView mCounterTextView;
+    private View mParentView;
 
     public UnreadCounterBadgeView(Context context) {
         super(context);
@@ -50,14 +64,16 @@ public class UnreadCounterBadgeView extends RelativeLayout {
     private void init() {
         inflate(getContext(), R.layout.unread_counter_badge, this);
         mCounterTextView = (TextView) findViewById(R.id.unread_counter_badge_text_view);
+        mParentView = findViewById(R.id.unread_counter_badge_layout);
     }
 
     /**
-     * Update the badge value
+     * Update the badge value and its status
      *
      * @param value the new value
+     * @param status the new status
      */
-    public void updateCounter(int value) {
+    public void updateCounter(int value, @Status int status) {
         if (value > 0) {
             if (value > 999) {
                 mCounterTextView.setText("999+");
@@ -65,6 +81,14 @@ public class UnreadCounterBadgeView extends RelativeLayout {
                 mCounterTextView.setText(value + "");
             }
             setVisibility(View.VISIBLE);
+
+            if (status == HIGHLIGHTED) {
+                mParentView.setBackgroundResource(R.drawable.unread_counter_badge_background_highlighted);
+            } else if (status == NOTIFIED) {
+                mParentView.setBackgroundResource(R.drawable.unread_counter_badge_background_notified);
+            } else { //if (status == DEFAULT)
+                mParentView.setBackgroundResource(R.drawable.unread_counter_badge_background_default);
+            }
         } else {
             setVisibility(View.INVISIBLE);
         }
