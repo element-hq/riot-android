a9f80467338bfa8e88f5e027ac4fb143fcf2a998
sabrinaj <sabrinajeanne@gmail.com>
2017-04-13 11:53:15 +0200

    Add javadoc

--- vector/src/main/java/im/vector/adapters/AdapterSection.java
+++ vector/src/main/java/im/vector/adapters/AdapterSection.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 Vector Creations Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package im.vector.adapters;
 
 import android.text.TextUtils;
@@ -46,6 +62,12 @@ public class AdapterSection<T> {
         updateTitle();
     }
 
+    /**
+     * Update items list
+     *
+     * @param items
+     * @param currentFilterPattern
+     */
     public void setItems(List<T> items, CharSequence currentFilterPattern) {
         if (mComparator != null) {
             Collections.sort(items, mComparator);
@@ -56,14 +78,12 @@ public class AdapterSection<T> {
         setFilteredItems(items, currentFilterPattern);
     }
 
-    private void updateTitle() {
-        if (getNbItems() > 0) {
-            mTitleFormatted = mTitle.concat(" (" + getNbItems() + ")");
-        } else {
-            mTitleFormatted = mTitle;
-        }
-    }
-
+    /**
+     * Update the filtered list of items using the given items and pattern
+     *
+     * @param items
+     * @param currentFilterPattern
+     */
     public void setFilteredItems(List<T> items, CharSequence currentFilterPattern) {
         mFilteredItems.clear();
         mFilteredItems.addAll(items);
@@ -72,49 +92,112 @@ public class AdapterSection<T> {
         updateTitle();
     }
 
+    /**
+     * Update the title depending on the number of items
+     */
+    private void updateTitle() {
+        if (getNbItems() > 0) {
+            mTitleFormatted = mTitle.concat(" (" + getNbItems() + ")");
+        } else {
+            mTitleFormatted = mTitle;
+        }
+    }
+
+    /**
+     * Get title
+     *
+     * @return
+     */
     public String getTitle() {
         return mTitleFormatted;
     }
 
-    public String getEmptyViewPlaceholder() {
-        return TextUtils.isEmpty(mCurrentFilterPattern) ? mNoItemPlaceholder : mNoResultPlaceholder;
-    }
-
+    /**
+     * Get the layout resId of the custom view that should be added to the header
+     *
+     * @return
+     */
     public int getHeaderSubView() {
         return mHeaderSubView;
     }
 
+    /**
+     * Get the text to display when there is no item for the section
+     *
+     * @return
+     */
+    public String getEmptyViewPlaceholder() {
+        return TextUtils.isEmpty(mCurrentFilterPattern) ? mNoItemPlaceholder : mNoResultPlaceholder;
+    }
 
+    /**
+     * Set the text to display when there is no item/no result
+     *
+     * @param noItemPlaceholder
+     */
     public void setEmptyViewPlaceholder(final String noItemPlaceholder) {
         mNoItemPlaceholder = noItemPlaceholder;
         mNoResultPlaceholder = noItemPlaceholder;
     }
 
+    /**
+     * Set the texts to display when there is no item or no result
+     *
+     * @param noItemPlaceholder
+     */
     public void setEmptyViewPlaceholder(final String noItemPlaceholder, final String noResultPlaceholder) {
         mNoItemPlaceholder = noItemPlaceholder;
         mNoResultPlaceholder = noResultPlaceholder;
     }
 
+    /**
+     * Get the header view type for the header (used by adapter)
+     *
+     * @return
+     */
     public int getHeaderViewType() {
         return mHeaderViewType;
     }
 
+    /**
+     * Get the content view type for the header (used by adapter)
+     *
+     * @return
+     */
     public int getContentViewType() {
         return mContentViewType;
     }
 
+    /**
+     * Get the list of items
+     *
+     * @return
+     */
     public List<T> getItems() {
         return mItems;
     }
 
+    /**
+     * Get the list of items matching the current filter
+     *
+     * @return
+     */
     public List<T> getFilteredItems() {
         return mFilteredItems;
     }
 
+    /**
+     * Get the number of items matching the current filter
+     *
+     * @return
+     */
     public int getNbItems() {
         return mFilteredItems.size();
     }
 
+    /**
+     * Update the filtered list by removing the current filter
+     */
     public void resetFilter() {
         mFilteredItems.clear();
         mFilteredItems.addAll(mItems);
@@ -123,14 +206,30 @@ public class AdapterSection<T> {
         updateTitle();
     }
 
+    /**
+     * Set whether the section should be hidden when it has no item
+     *
+     * @return
+     */
     public void setIsHiddenWhenEmpty(final boolean isHiddenWhenEmpty) {
         mIsHiddenWhenEmpty = isHiddenWhenEmpty;
     }
 
+    /**
+     * Get whether the section should be hidden when it has no item
+     *
+     * @return
+     */
     public boolean hideWhenEmpty() {
         return mIsHiddenWhenEmpty;
     }
 
+    /**
+     * Remove an item from the section
+     *
+     * @param object
+     * @return
+     */
     public boolean removeItem(final T object) {
         if (mFilteredItems.contains(object)) {
             mFilteredItems.remove(object);
