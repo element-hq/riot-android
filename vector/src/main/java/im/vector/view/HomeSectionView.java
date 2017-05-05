f5468891162c11f0af9339b7b76ab00e21e5030d
sabrinaj <sabrinajeanne@gmail.com>
2017-05-05 17:05:25 +0200

    Home: scroll to beginning when tap on header

--- vector/src/main/java/im/vector/view/HomeSectionView.java
+++ vector/src/main/java/im/vector/view/HomeSectionView.java
@@ -26,6 +26,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.Filter;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -107,6 +108,16 @@ public class HomeSectionView extends RelativeLayout {
         shape.setCornerRadius(100);
         shape.setColor(ContextCompat.getColor(getContext(), R.color.vector_white_alpha_50));
         mBadge.setBackground(shape);
+
+        mHeader.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mRecyclerView != null) {
+                    mRecyclerView.stopScroll();
+                    mRecyclerView.scrollToPosition(0);
+                }
+            }
+        });
     }
 
     /**
