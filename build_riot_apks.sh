eb33c997efb80b764116d8f8089af4a7bf349651
ylecollen <ylecollen@amdocs.com>
2017-05-29 13:52:14 +0200

    Update the gradle to ensure that the Fdroid flavor won't compile with com.google.gms.google-services.
    Fix https://github.com/vector-im/riot-android/issues/1175

--- build_riot_apks.sh
+++ build_riot_apks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm *.apk
-./gradlew clean build
+./gradlew clean lintAppRelease lintAppfdroidRelease assembleAppMatrixorg assembleAppfdroidMatrixorg
 #cp app/build/outputs/apk/app-alpha-matrixorg.apk ./alpha.apk
 cp vector/build/outputs/apk/vector-app-matrixorg.apk ./riotGooglePlay.apk
 cp vector/build/outputs/apk/vector-appfdroid-matrixorg.apk ./riotFDroid.apk
