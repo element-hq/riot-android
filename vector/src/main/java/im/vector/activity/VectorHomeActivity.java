a551f4856781460640dbbc4f5e9d4309647a63ac
ylecollen <ylecollen@amdocs.com>
2017-06-16 14:31:35 +0200

    ux_rework Add crash desciption in the bug report.

--- vector/src/main/java/im/vector/activity/VectorHomeActivity.java
+++ vector/src/main/java/im/vector/activity/VectorHomeActivity.java
@@ -570,6 +570,7 @@ public class VectorHomeActivity extends AppCompatActivity implements SearchView.
             }).setNegativeButton(getString(R.string.no), new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
+                    BugReporter.deleteCrashFile(VectorHomeActivity.this);
                 }
             }).show();
 
