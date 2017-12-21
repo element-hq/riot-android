748058bc8a4d79ba9d912c40cefae54f62651829
ylecollen <ylecollen@amdocs.com>
2017-12-21 09:44:45 +0100

    add batch file to set the develop environment dev

---
+++ set_debug_env.sh
@@ -0,0 +1,25 @@
+echo updir
+cd ..
+
+echo remove sdk folder
+rm -rf matrix-android-sdk
+
+echo clone the git folder
+git clone -b develop https://github.com/matrix-org/matrix-android-sdk 
+
+echo go to riot folder
+cd riot-android
+
+echo replace step 1
+sed -i '' -e 's/\/\/include/include/' settings.gradle
+sed -i '' -e 's/\/\/project/project/' settings.gradle
+
+echo replace step 2
+sed -i '' -e "s/compile(name: 'matrix/\/\/compile(name: 'matrix/" vector/build.gradle
+sed -i '' -e "s/\/\/compile project(':matrix-/compile project(':matrix-/" vector/build.gradle
+
+echo remove the SDK lib
+rm -f vector/libs/matrix-sdk.aar
+
+
+
