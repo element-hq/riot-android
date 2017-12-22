3c68293b0613ab3b06b12d989fd3a1213c1ba605
ylecollen <ylecollen@amdocs.com>
2017-12-22 16:24:49 +0100

    use a tmp folder to clone the matrix-android-skd git repo

--- trigger_release.sh
+++ trigger_release.sh
@@ -4,12 +4,12 @@ echo updir
 cd ..
 
 echo remove sdk folder
-rm -rf matrix-android-sdk
+rm -rf matrix-android-sdk_build_release
 
 echo clone the git folder
-git clone -b master https://github.com/matrix-org/matrix-android-sdk 
+git clone -b master https://github.com/matrix-org/matrix-android-sdk matrix-android-sdk_build_release
 
-cd matrix-android-sdk 
+cd matrix-android-sdk_build_release
 
 ./gradlew clean assembleRelease
 ./gradlew releaseDocs
@@ -18,10 +18,10 @@ git push --porcelain --progress --recurse-submodules=check origin refs/heads/mas
 
 cd ../riot-android
 rm -f vector/libs/matrix-sdk.aar 
-cp ../matrix-android-sdk/matrix-sdk/build/outputs/aar/matrix-sdk-release-*.aar vector/libs/matrix-sdk.aar 
+cp ../matrix-android-sdk_build_release/matrix-sdk/build/outputs/aar/matrix-sdk-release-*.aar vector/libs/matrix-sdk.aar 
 
 git commit -m "auto-generated from script" vector/libs/matrix-sdk.aar 
 git push --porcelain --progress --recurse-submodules=check origin refs/heads/master:refs/heads/master
 
-
+rm -rf matrix-android-sdk_build_release
 
