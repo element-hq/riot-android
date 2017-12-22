3897d07fe4f075cd8db8a522654653ca8b244b2e
ylecollen <ylecollen@amdocs.com>
2017-12-22 09:37:52 +0100

    Restore the genuine sh script

--- build_riot_apks.sh
+++ build_riot_apks.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-cd ..
-
-echo remove sdk folder
-rm -rf matrix-android-sdk
-
-echo clone the git folder
-git clone -b master https://github.com/matrix-org/matrix-android-sdk 
-
-cd matrix-android-sdk
-./gradlew clean assembleRelease
-
-cd ../riot-android
-rm -f vector/libs/matrix-sdk.aar 
-cp ../matrix-android-sdk/matrix-sdk/build/outputs/aar/matrix-sdk-release-*.aar vector/libs/matrix-sdk.aar 
 
 rm *.apk
 ./gradlew clean 
