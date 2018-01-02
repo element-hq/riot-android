acb76bb6ae1f357437e26f3ac8db2afda506159e
ylecollen <ylecollen@amdocs.com>
2018-01-02 14:33:03 +0100

    Update debug sh script

--- set_debug_env.sh
+++ set_debug_env.sh
@@ -9,9 +9,9 @@ echo clone the git folder
 git clone -b develop https://github.com/matrix-org/matrix-android-sdk ../matrix-android-sdk
 
 echo replace step 1
-sed -i '' -e 's/\/\/include/include/' settings.gradle
-sed -i '' -e 's/\/\/project/project/' settings.gradle
+sed -i '' -e 's/\/\/include/include/' settings.gradle || true
+sed -i '' -e 's/\/\/project/project/' settings.gradle || true
 
 echo replace step 2
-sed -i '' -e "s/compile(name: 'matrix/\/\/compile(name: 'matrix/" vector/build.gradle
-sed -i '' -e "s/\/\/compile project(':matrix-/compile project(':matrix-/" vector/build.gradle
+sed -i '' -e "s/compile(name: 'matrix/\/\/compile(name: 'matrix/" vector/build.gradle || true
+sed -i '' -e "s/\/\/compile project(':matrix-/compile project(':matrix-/" vector/build.gradle || true
