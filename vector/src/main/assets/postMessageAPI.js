09918bcb7dbcd3e3f0004bf719698ed0ccf26c84
Benoit Marty <benoit.marty@gmail.com>
2018-05-18 18:21:38 +0200

    Bridge API: ensure event from Widget provide an ID

--- vector/src/main/assets/postMessageAPI.js
+++ vector/src/main/assets/postMessageAPI.js
@@ -5,6 +5,18 @@ var sendObjectMessageToRiotAndroid = function(parameters) {
 };
 
 var onWidgetMessageToRiotAndroid = function(event) {
+    /* Use an internal "_id" field for matching onMessage events and requests
+       _id was originally used by the Modular API. Keep it */
+    if (!event.data._id) {
+        /* The Matrix Widget API v2 spec says:
+           "The requestId field should be unique and included in all requests" */
+        event.data._id = event.data.requestId;
+    }
+    /* Make sure to have one id */
+    if (!event.data._id) {
+        event.data._id = Date.now() + "-" + Math.random().toString(36);
+    }
+
     console.log("onWidgetMessageToRiotAndroid " + event.data._id);
 
     if (android_widget_events[event.data._id]) {
