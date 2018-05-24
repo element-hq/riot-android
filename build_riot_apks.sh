85f24649f046c52efcc504f00556deb9aa6a45a2
giomfo <giom@matrix.org>
2018-05-24 10:40:21 +0200

    Update apk paths in build_riot_apks.sh

--- build_riot_apks.sh
+++ build_riot_apks.sh
@@ -6,5 +6,5 @@ rm *.apk
 ./gradlew lintAppfdroidRelease assembleAppfdroidMatrixorg
 
 #cp app/build/outputs/apk/app-alpha-matrixorg.apk ./alpha.apk
-cp vector/build/outputs/apk/vector-app-matrixorg.apk ./riotGooglePlay.apk
-cp vector/build/outputs/apk/vector-appfdroid-matrixorg.apk ./riotFDroid.apk
+cp vector/build/outputs/apk/app/matrixorg/vector-app-matrixorg.apk ./riotGooglePlay.apk
+cp vector/build/outputs/apk/appfdroid/matrixorg/vector-appfdroid-matrixorg.apk ./riotFDroid.apk
