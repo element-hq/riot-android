9d057ac0d1c8759115ea310c640f5206058c6f38
ganfra <francois.ganard@gmail.com>
2018-08-07 19:05:30 +0200

    WeakReferenceDelegate : clean unnecessary code

--- vector/src/main/java/im/vector/util/WeakReferenceDelegate.kt
+++ vector/src/main/java/im/vector/util/WeakReferenceDelegate.kt
@@ -19,18 +19,13 @@ package im.vector.util
 import java.lang.ref.WeakReference
 import kotlin.reflect.KProperty
 
-fun <T> weak() = WeakReferenceDelegate<T>()
 fun <T> weak(value: T) = WeakReferenceDelegate(value)
 
-class WeakReferenceDelegate<T> {
-    private var weakReference: WeakReference<T>? = null
+class WeakReferenceDelegate<T>(value: T) {
 
-    constructor()
-    constructor(value: T) {
-        weakReference = WeakReference(value)
-    }
+    private var weakReference: WeakReference<T> = WeakReference(value)
 
-    operator fun getValue(thisRef: Any, property: KProperty<*>): T? = weakReference?.get()
+    operator fun getValue(thisRef: Any, property: KProperty<*>): T? = weakReference.get()
     operator fun setValue(thisRef: Any, property: KProperty<*>, value: T) {
         weakReference = WeakReference(value)
     }
