592d55059190d2121afbac937bedbbf30dd0563d
Benoit Marty <benoit.marty@gmail.com>
2018-08-28 10:20:12 +0200

    Fix exception detected in RageShake

--- vector/src/main/java/im/vector/fragments/GroupDetailsPeopleFragment.java
+++ vector/src/main/java/im/vector/fragments/GroupDetailsPeopleFragment.java
@@ -1,7 +1,8 @@
 /*
  * Copyright 2016 OpenMarket Ltd
  * Copyright 2017 Vector Creations Ltd
- * 
+ * Copyright 2018 New Vector Ltd
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -117,6 +118,10 @@ public class GroupDetailsPeopleFragment extends GroupDetailsBaseFragment {
 
     @Override
     public void refreshViews() {
+        if (!isAdded()) {
+            return;
+        }
+
         mAdapter.setJoinedGroupUsers(mActivity.getGroup().getGroupUsers().getUsers());
         mAdapter.setInvitedGroupUsers(mActivity.getGroup().getInvitedGroupUsers().getUsers());
     }
