542d9081caaa7d0f92c4521be30b815ec7db65a1
Benoit Marty <benoit@matrix.org>
2019-01-22 10:19:41 +0100

    Provide theme in rageshake data

--- vector/src/main/java/im/vector/util/BugReporter.java
+++ vector/src/main/java/im/vector/util/BugReporter.java
@@ -21,7 +21,6 @@ package im.vector.util;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Build;
@@ -56,6 +55,7 @@ import im.vector.VectorApp;
 import im.vector.activity.BugReportActivity;
 import im.vector.extensions.BasicExtensionsKt;
 import im.vector.settings.VectorLocale;
+import im.vector.ui.themes.ThemeUtils;
 import okhttp3.Call;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -228,7 +228,8 @@ public class BugReporter {
                                     + Build.VERSION.INCREMENTAL + "-" + Build.VERSION.CODENAME)
                             .addFormDataPart("locale", Locale.getDefault().toString())
                             .addFormDataPart("app_language", VectorLocale.INSTANCE.getApplicationLocale().toString())
-                            .addFormDataPart("default_app_language", SystemUtilsKt.getDeviceLocale(context).toString());
+                            .addFormDataPart("default_app_language", SystemUtilsKt.getDeviceLocale(context).toString())
+                            .addFormDataPart("theme", ThemeUtils.INSTANCE.getApplicationTheme(context));
 
                     String buildNumber = context.getString(R.string.build_number);
                     if (!TextUtils.isEmpty(buildNumber) && !buildNumber.equals("0")) {
@@ -268,14 +269,8 @@ public class BugReporter {
 
                     mScreenshot = null;
 
-                    // add some github tags
-                    try {
-                        PackageInfo pInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
-                        builder.addFormDataPart("label", pInfo.versionName);
-                    } catch (Exception e) {
-                        Log.e(LOG_TAG, "## sendBugReport() : cannot retrieve the appname " + e.getMessage(), e);
-                    }
-
+                    // add some github labels
+                    builder.addFormDataPart("label", BuildConfig.VERSION_NAME);
                     builder.addFormDataPart("label", BuildConfig.FLAVOR_DESCRIPTION);
                     builder.addFormDataPart("label", context.getString(R.string.git_branch_name));
 
