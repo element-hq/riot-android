85e2f26745a63fd818981132b8966cf5cf82221c
Valere <valeref@matrix.org>
2019-02-21 14:53:04 +0100

    Removed beta warning for e2e in room settings

--- vector/src/main/java/im/vector/fragments/VectorRoomSettingsFragment.java
+++ vector/src/main/java/im/vector/fragments/VectorRoomSettingsFragment.java
@@ -1825,54 +1825,37 @@ public class VectorRoomSettingsFragment extends PreferenceFragmentCompat impleme
                     @Override
                     public boolean onPreferenceChange(Preference preference, Object newValueAsVoid) {
                         boolean newValue = (boolean) newValueAsVoid;
-
                         if (newValue != mRoom.isEncrypted()) {
-                            new AlertDialog.Builder(getActivity())
-                                    .setTitle(R.string.room_settings_addresses_e2e_prompt_title)
-                                    .setMessage(R.string.room_settings_addresses_e2e_prompt_message)
-                                    .setPositiveButton(R.string.room_settings_addresses_e2e_encryption_enable_encryption,
-                                            new DialogInterface.OnClickListener() {
-                                                @Override
-                                                public void onClick(DialogInterface dialog, int which) {
-                                                    displayLoadingView();
+                            displayLoadingView();
 
-                                                    mRoom.enableEncryptionWithAlgorithm(CryptoConstantsKt.MXCRYPTO_ALGORITHM_MEGOLM, new ApiCallback<Void>() {
+                            mRoom.enableEncryptionWithAlgorithm(CryptoConstantsKt.MXCRYPTO_ALGORITHM_MEGOLM, new ApiCallback<Void>() {
 
-                                                        private void onDone() {
-                                                            hideLoadingView(false);
-                                                            refreshEndToEnd();
-                                                        }
+                                private void onDone() {
+                                    hideLoadingView(false);
+                                    refreshEndToEnd();
+                                }
 
-                                                        @Override
-                                                        public void onSuccess(Void info) {
-                                                            onDone();
-                                                        }
+                                @Override
+                                public void onSuccess(Void info) {
+                                    onDone();
+                                }
 
-                                                        @Override
-                                                        public void onNetworkError(Exception e) {
-                                                            onDone();
-                                                        }
+                                @Override
+                                public void onNetworkError(Exception e) {
+                                    onDone();
+                                }
 
-                                                        @Override
-                                                        public void onMatrixError(MatrixError e) {
-                                                            onDone();
-                                                        }
+                                @Override
+                                public void onMatrixError(MatrixError e) {
+                                    onDone();
+                                }
 
-                                                        @Override
-                                                        public void onUnexpectedError(Exception e) {
-                                                            onDone();
-                                                        }
-                                                    });
+                                @Override
+                                public void onUnexpectedError(Exception e) {
+                                    onDone();
+                                }
+                            });
 
-                                                }
-                                            })
-                                    .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialog, int which) {
-                                            encryptSwitchPreference.setChecked(false);
-                                        }
-                                    })
-                                    .show();
                         }
                         return true;
                     }
