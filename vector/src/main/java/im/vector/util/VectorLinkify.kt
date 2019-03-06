cf015447abac6aa9ff8b6272084282a81cc56e2b
Valere <valeref@matrix.org>
2019-03-06 11:14:43 +0100

    Fix / prune overlapping spans

--- vector/src/main/java/im/vector/util/VectorLinkify.kt
+++ vector/src/main/java/im/vector/util/VectorLinkify.kt
@@ -18,46 +18,56 @@ package im.vector.util
 import android.support.v4.text.util.LinkifyCompat
 import android.text.Spannable
 import android.text.SpannableString
-import android.text.Spanned
 import android.text.method.LinkMovementMethod
 import android.text.style.URLSpan
 import android.text.util.Linkify
 import android.widget.TextView
+import java.util.*
 
 /**
  * Better support for auto link than the default implementation
  */
 fun TextView.vectorCustomLinkify() {
 
-    val currentSpan = SpannableString.valueOf(text)
+    val spannableText = SpannableString.valueOf(text)
 
     //Use the framework first, the found span can then be manipulated if needed
-    LinkifyCompat.addLinks(currentSpan, Linkify.WEB_URLS or Linkify.EMAIL_ADDRESSES)
+    LinkifyCompat.addLinks(spannableText, Linkify.WEB_URLS or Linkify.EMAIL_ADDRESSES or Linkify.PHONE_NUMBERS)
 
     //we might want to modify some matches
     val createdSpans = ArrayList<LinkSpec>()
-    currentSpan.forEachSpanIndexed { _, urlSpan, start, end ->
-        currentSpan.removeSpan(urlSpan)
+    spannableText.forEachSpanIndexed { _, urlSpan, start, end ->
+        spannableText.removeSpan(urlSpan)
+
+        //remove short PN, too much false positive
+        if (urlSpan.url?.startsWith("tel:") == true) {
+            if (end - start > 6) { //Do not match under 7 digit
+                createdSpans.add(LinkSpec(URLSpan(urlSpan.url), start, end))
+            }
+            return@forEachSpanIndexed
+        }
+
+
         //check trailing space
-        if (end < currentSpan.length - 1 && currentSpan[end] == '/') {
+        if (end < spannableText.length - 1 && spannableText[end] == '/') {
             //modify the span to include the slash
             val spec = LinkSpec(URLSpan(urlSpan.url + "/"), start, end + 1)
             createdSpans.add(spec)
             return@forEachSpanIndexed
         }
         //Try to do something for ending ) issues/3020
-        if (currentSpan[end - 1] == ')') {
+        if (spannableText[end - 1] == ')') {
             var lbehind = end - 2
             var isFullyContained = 1
             while (lbehind > start) {
-                val char = currentSpan[lbehind]
+                val char = spannableText[lbehind]
                 if (char == '(') isFullyContained -= 1
                 if (char == ')') isFullyContained += 1
                 lbehind--
             }
             if (isFullyContained != 0) {
                 //In this case we will return false to match, and manually add span if we want?
-                val span = URLSpan(currentSpan.substring(start, end - 1))
+                val span = URLSpan(spannableText.substring(start, end - 1))
                 val spec = LinkSpec(span, start, end - 1)
                 createdSpans.add(spec)
                 return@forEachSpanIndexed
@@ -67,33 +77,76 @@ fun TextView.vectorCustomLinkify() {
         createdSpans.add(LinkSpec(URLSpan(urlSpan.url), start, end))
     }
 
+    LinkifyCompat.addLinks(spannableText, VectorAutoLinkPatterns.GEO_URI, "geo:", arrayOf("geo:"), geoMatchFilter, null)
+    spannableText.forEachSpanIndexed { _, urlSpan, start, end ->
+        spannableText.removeSpan(urlSpan)
+        createdSpans.add(LinkSpec(URLSpan(urlSpan.url), start, end))
+    }
+
+    pruneOverlaps(createdSpans)
     for (spec in createdSpans) {
-        currentSpan.setSpan(spec.span, spec.start, spec.end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableText.setSpan(spec.span, spec.start, spec.end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
     }
 
-    LinkifyCompat.addLinks(currentSpan, VectorAutoLinkPatterns.GEO_URI, "geo:", arrayOf("geo:"), geoMatchFilter, null)
-
-    //this is a bit hacky but for phone numbers we use the framework but it's too lenient
-    val spannable = SpannableString(text) //must be a new one
-    Linkify.addLinks(spannable, Linkify.PHONE_NUMBERS)
-    val pnSpan = spannable.getSpans(0, spannable.length, URLSpan::class.java)
-    for (span in pnSpan) {
-        val start = spannable.getSpanStart(span)
-        val end = spannable.getSpanEnd(span)
-        if (end - start > 6) { //Do not match under 7 digit
-            currentSpan.setSpan(span, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-        }
-    }
-
-    //maybe need to prune overlaps? tried to make some but didn't find
-    text = currentSpan
-
+    text = spannableText
     addLinkMovementMethod()
 }
+
+private fun pruneOverlaps(links: ArrayList<LinkSpec>) {
+    Collections.sort(links, COMPARATOR)
+    var len = links.size
+    var i = 0
+    while (i < len - 1) {
+        val a = links[i]
+        val b = links[i + 1]
+        var remove = -1
+
+        //test if there is an overlap
+        if (b.start in a.start..(a.end - 1)) {
+            if (b.end <= a.end) {
+                //b is inside a -> b should be removed
+                remove = i + 1
+            } else if (a.end - a.start > b.end - b.start) {
+                //overlap and a is bigger -> b should be removed
+                remove = i + 1
+            } else if (a.end - a.start < b.end - b.start) {
+                //overlap and a is smaller -> a should be removed
+                remove = i
+            }
+
+            if (remove != -1) {
+                val spec = links[remove]
+                links.removeAt(remove)
+                len--
+                continue
+            }
+        }
+        i++
+    }
+}
+
 
 private data class LinkSpec(val span: URLSpan,
                             val start: Int,
                             val end: Int)
+
+private val COMPARATOR = Comparator<LinkSpec> { (_, startA, endA), (_, startB, endB) ->
+    if (startA < startB) {
+        return@Comparator -1
+    }
+
+    if (startA > startB) {
+        return@Comparator 1
+    }
+
+    if (endA < endB) {
+        return@Comparator 1
+    }
+
+    if (endA > endB) {
+        -1
+    } else 0
+}
 
 //Exclude short match that don't have geo: prefix, e.g do not highlight things like 1,2
 private val geoMatchFilter = Linkify.MatchFilter { s, start, end ->
