fcd86ca9c37cd832bf57488fb4f98c3b64bad0fd
AmirAli Akbari <amiraliakbari@gmail.com>
2019-04-25 10:55:43 +0430

    Improving settings

---
+++ vector/src/saba/java/im/vector/preference/SabaListPreference.kt
@@ -0,0 +1,19 @@
+package im.vector.preference
+
+import android.content.Context
+import android.support.v7.preference.ListPreference
+import android.util.AttributeSet
+
+
+class SabaListPreference : ListPreference {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    init {
+        setVisible(false)
+    }
+}
