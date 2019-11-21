874fd637cfada45874a8dc58e26fed5b42e2978c
Valere <valeref@matrix.org>
2019-11-21 11:41:23 +0100

    Infinite restart loop after expired token error
    Fixes #3249

--- vector/src/main/java/im/vector/store/LoginStorage.java
+++ vector/src/main/java/im/vector/store/LoginStorage.java
@@ -199,6 +199,6 @@ public class LoginStorage {
         SharedPreferences prefs = mContext.getSharedPreferences(PREFS_LOGIN, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
         editor.remove(PREFS_KEY_CONNECTION_CONFIGS);
-        editor.apply();
+        editor.commit();
     }
 }
