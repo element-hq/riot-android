4afb530a09dfa5f1d524240355f1ea6158994030
Maryam Behzadi <mary.behzi@gmail.com>
2019-11-30 13:44:30 +0330

    translations and AboutSaba Buttons

--- vector/src/saba/java/im/vector/activity/AboutSabaActivity.kt
+++ vector/src/saba/java/im/vector/activity/AboutSabaActivity.kt
@@ -1,7 +1,10 @@
 package im.vector.activity
 
 //import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 
 import im.vector.R
 import android.widget.TextView
@@ -21,11 +24,33 @@ class AboutSabaActivity : AppCompatActivity() {
     @BindView(R.id.security_level_text)
     lateinit var securityLevelTextView: TextView
 
+    @BindView(R.id.button)
+    lateinit var aboutsababutton: Button
+
+    @BindView(R.id.button2)
+    lateinit var freesoftwarebutton: Button
+
+    @BindView(R.id.button3)
+    lateinit var privacybutton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_saba)
         ButterKnife.bind(this)
         versionTextView.text = getString(R.string.saba_about_version_info, BuildConfig.VERSION_NAME)
         securityLevelTextView.text = getString(R.string.saba_about_security_level_info, "1")
+        aboutsababutton.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.sabaos.com"))
+            startActivity(i)
+        }
+        freesoftwarebutton.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.sabaos.com/legal"))
+            startActivity(i)
+        }
+        privacybutton.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.sabaos.com/privacy"))
+            startActivity(i)
+        }
+
     }
 }
