da59448b130b2f2f72b86cb236c2afdaec94db30
Rafi <rsadat@datagene.com.au>
2020-07-09 11:49:09 +1000

    blank recyclerview

--- vector/src/main/java/im/vector/directory/role/detail/RoleDetailActivity.kt
+++ vector/src/main/java/im/vector/directory/role/detail/RoleDetailActivity.kt
@@ -1,10 +1,27 @@
 package im.vector.directory.role.detail
 
+import androidx.fragment.app.FragmentManager
 import im.vector.R
 import im.vector.activity.AbstractWidgetActivity
+import im.vector.activity.MXCActionBarActivity
 import im.vector.activity.VectorAppCompatActivity
 
-class RoleDetailActivity: VectorAppCompatActivity(){
+class RoleDetailActivity: MXCActionBarActivity(), FragmentManager.OnBackStackChangedListener {
     override fun getLayoutRes(): Int = R.layout.activity_role_detail
+    override fun getTitleRes() = R.string.title_activity_role_detail
 
+    override fun initUiAndData() {
+        configureToolbar()
+    }
+
+    override fun onDestroy() {
+        supportFragmentManager.removeOnBackStackChangedListener(this)
+        super.onDestroy()
+    }
+
+    override fun onBackStackChanged() {
+        if (0 == supportFragmentManager.backStackEntryCount) {
+            supportActionBar?.title = getString(getTitleRes())
+        }
+    }
 }
